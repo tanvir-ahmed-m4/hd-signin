a7ab7f2fa637d128506829bbcd5ea03894517fc7
Galen <galenthegreater@gmail.com>
2014-08-22 21:39:50 -0500

    Added support for signing in by hitting "enter"

--- src/main/webapp/js/signin/app.js
+++ src/main/webapp/js/signin/app.js
@@ -8,3 +8,17 @@ angular.module('signin').config(
 			.otherwise({redirectTo:'/'});
 		});
 
+
+angular.module('signin').directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
