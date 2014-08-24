6b40d650c2c4c1818ac3e7dbe8dcf1dbf870b53a
Galen <galenthegreater@gmail.com>
2014-08-24 12:45:41 -0500

    Minor code fixes

--- src/main/webapp/js/admin/editemployeectrl.js
+++ src/main/webapp/js/admin/editemployeectrl.js
@@ -1,21 +1,35 @@
 angular.module('admin').controller('EmployeeMgmtCtrl', ['$scope', 'employeeServices',  function($scope, employeeServices){
-	// TODO load via AJAX
+	// TODO load employee types via AJAX
 	$scope.employeeTypes = 	['SCC', 'SCC_LEAD', 'SUPERVISOR', 'SYSADMIN'];
 	
-	$scope.signedInEmployees = [];
-
 	$scope.employees = [];
 
 	$scope.activeEmployee = {'firstName': 'Loading...'};
 
+	function addEmployees(employees){
+		for(var i = 0; i < employees.length; i++){
+			addEmployee(employees[i]);
+		}
+	}
+	
+	function addEmployee(employee){
+		for(var i = 0; i < $scope.employees.length; i++){
+			if($scope.employees[i].id == employee.id){
+				$scope.employees[i] = employee;
+				return;
+			}
+		}
+		$scope.employees.push(employee);
+	}
+	
 	$scope.saveChanges = function(){
 		console.log('Saving ' + JSON.stringify($scope.activeEmployee));
 		employeeServices.updateEmployee($scope.activeEmployee);
-		
+		addEmployee($scope.activeEmployee);
 	}
 	
 	employeeServices.getAllEmployees().then(function(response){
-		$scope.employees = response;
-		$scope.activeEmployee = response[0];
+		addEmployees(response);
+		$scope.activeEmployee = $scope.employees[0];
 	});
 }]);
