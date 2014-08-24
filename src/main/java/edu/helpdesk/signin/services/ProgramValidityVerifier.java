86bab2171bb830805244425122cdf3bb4af758be
Galen <galenthegreater@gmail.com>
2014-08-24 01:26:31 -0500

    Added support for creating multiple sysadmins at program launch

--- src/main/java/edu/helpdesk/signin/services/ProgramValidityVerifier.java
+++ src/main/java/edu/helpdesk/signin/services/ProgramValidityVerifier.java
@@ -16,51 +16,63 @@ public class ProgramValidityVerifier {
 	private static final Logger log = LoggerFactory.getLogger(ProgramValidityVerifier.class);
 
 	@Value("${sysadmin.netid}")
-	private String netid;
+	private String netids;
 
 	@Autowired
 	private EmployeeDao employeeDao;
 
 	@Autowired
 	private SigninMapper smapper;
-	
+
 	public ProgramValidityVerifier() {}
-	
+
 	@PostConstruct
 	public void validate(){
 		try{
 			log.debug("Validating program integrity...");
-
-			Employee sysadmin = employeeDao.getEmployeeByNetId(netid);
+			String[] netIds = netids.split(",");
 			
-			if(sysadmin == null){
-				sysadmin = new Employee();
-				sysadmin.setNetId(netid);
-				sysadmin.setRiceId("INVALID");
-				sysadmin.setFirstName("SYSADMIN");
-				sysadmin.setLastName("SYSADMIN");
-				sysadmin.setIsEmployeeActive(true);
-				sysadmin.setEmployeeType(EmployeeType.SYSADMIN);
-				log.warn("Employee with net ID {} does not exist, but is "
-						+ "defined as a Systems Administrator in the "
-						+ "configuration file. Now creating a new eployee with "
-						+ "the given net ID and the privilege level '{}'",
-						netid, EmployeeType.SYSADMIN);
-				employeeDao.createEmployee(sysadmin);
+			for(int i = 0; i < netIds.length; i++){
+				netIds[i] = netIds[i].trim();
 			}
-			else{
-				if(!EmployeeType.SYSADMIN.equals(sysadmin.getEmployeeType())){
-					log.warn("Employee with net ID '{}', who is defined as a "
-							+ "Systems Administrator in the configuration file, "
-							+ "currently only has privilege level '{}'. Now "
-							+ "promoting to '{}'", 
-							netid, sysadmin.getEmployeeType(), 
-							EmployeeType.SYSADMIN);
-					sysadmin.setEmployeeType(EmployeeType.SYSADMIN);
-					employeeDao.updateEmployee(sysadmin);
+
+			for(int i = 0; i < netIds.length; i++){
+				String nid = netIds[i];
+				try{
+
+					Employee sysadmin = employeeDao.getEmployeeByNetId(nid);
+
+					if(sysadmin == null){
+						sysadmin = new Employee();
+						sysadmin.setNetId(nid);
+						sysadmin.setRiceId("INVALID:" + nid);
+						sysadmin.setFirstName("SYSADMIN: " + nid);
+						sysadmin.setLastName("SYSADMIN");
+						sysadmin.setIsEmployeeActive(true);
+						sysadmin.setEmployeeType(EmployeeType.SYSADMIN);
+						log.warn("Employee with net ID {} does not exist, but is "
+								+ "defined as a Systems Administrator in the "
+								+ "configuration file. Now creating a new eployee with "
+								+ "the given net ID and the privilege level '{}'",
+								nid, EmployeeType.SYSADMIN);
+						employeeDao.createEmployee(sysadmin);
+					}
+					else{
+						if(!EmployeeType.SYSADMIN.equals(sysadmin.getEmployeeType())){
+							log.warn("Employee with net ID '{}', who is defined as a "
+									+ "Systems Administrator in the configuration file, "
+									+ "currently only has privilege level '{}'. Now "
+									+ "promoting to '{}'", 
+									nid, sysadmin.getEmployeeType(), 
+									EmployeeType.SYSADMIN);
+							sysadmin.setEmployeeType(EmployeeType.SYSADMIN);
+							employeeDao.updateEmployee(sysadmin);
+						}
+					}
+				}catch(Exception e){
+					log.warn("Exception making sure sysadmin with netid {} exists", nid, e);
 				}
 			}
-			
 			log.info("Done validating program integrity");
 		}catch(Exception e){
 			log.error("Exception validating program", e);
