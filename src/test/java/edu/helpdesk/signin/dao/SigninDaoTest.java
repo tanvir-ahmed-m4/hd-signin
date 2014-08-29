a70ddb3ca37ebb4a73e9f3f8d579ed769ca79f6c
Galen <galenthegreater@gmail.com>
2014-08-29 16:00:41 -0500

    Added the first unit test

---
+++ src/test/java/edu/helpdesk/signin/dao/SigninDaoTest.java
@@ -0,0 +1,27 @@
+package edu.helpdesk.signin.dao;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/edu/helpdesk/signin/applicationContext.xml")
+@WebAppConfiguration
+public class SigninDaoTest {
+	
+	@Test
+	public void testSwipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllSignedInEmployees() {
+		fail("Not yet implemented");
+	}
+
+}
