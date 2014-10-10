8110c939ad4f98368bac640c683f7380de82721d
Galen <galenthegreater@gmail.com>
2014-10-10 10:55:24 -0500

    Externalized several of the configuration properties for the email service

--- src/main/java/edu/helpdesk/signin/services/EmailService.java
+++ src/main/java/edu/helpdesk/signin/services/EmailService.java
@@ -3,21 +3,43 @@ package edu.helpdesk.signin.services;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 public class EmailService {
 	private static final Logger log = LoggerFactory.getLogger(EmailService.class);
-
-	private boolean sendingEnabled = true;
-
-
-
+	private static EmailService INSTANCE = null;
+	
+	public static EmailService get(){
+		return INSTANCE;
+	}
+	
+	@Value("${email.from.name}")
+	private String emailFromName;
+	
+	@Value("${email.from.address}")
+	private String emailFromAddress;
+	
+	@Value("${email.hostname}")
+	private String emailHostname;
+	
+	@Value("${email.debugenabled}")
+	private boolean debugEnabled;
+	
+	@Value("${email.enabled}")
+	private boolean sendingEnabled;
+	
+	
 	public EmailService() {
-		// TODO Auto-generated constructor stub
+		if(INSTANCE != null){
+			throw new IllegalStateException("Can't instantiate an EmailService more than once; use the EmailService.get() static method to get an isntance of the class");
+		}
+		INSTANCE = this;
 	}
 
 	public HtmlEmail createEmail(List<String> to, String subject, String htmlBody){
@@ -31,17 +53,22 @@ public class EmailService {
 	public HtmlEmail createEmail(List<String> to, List<String> cc, List<String> bcc, String subject, String htmlBody){
 		HtmlEmail out = new HtmlEmail();
 		try{
-			out.setFrom("no-reply@rice.edu", "Help Desk Sign-In System");
-			out.setHostName("localhost");
-
+			out.setFrom(emailFromAddress, emailFromName);
+			out.setHostName(emailHostname);
+			out.addHeader("X-Files", "The Truth Is Out There");
+			
+			// The email content
 			out.setSubject(subject);
 			out.setHtmlMsg(htmlBody);
+			
+			// add the recipients
 			if(to != null && to.size() > 0)
 				out.addTo(to.toArray(new String[]{}));
 			if(cc != null && cc.size() > 0)
 				out.addCc(cc.toArray(new String[]{}));
 			if(bcc != null && bcc.size() > 0)
 				out.addBcc(bcc.toArray(new String[]{}));
+			
 		}catch(EmailException e){
 			log.warn("Exception creating email", e);
 			return null;
@@ -60,9 +87,9 @@ public class EmailService {
 		}
 		try {
 			log.debug("Sending email to {}...", email.getToAddresses());
-			//email.setDebug(true);
-			email.send();
-			log.debug("Email sent");
+			email.setDebug(debugEnabled);
+			String id = email.send();
+			log.debug("Email sent (id {})", id);
 			return true;
 		} catch (EmailException e) {
 			log.warn("Error sending email", e);
--- src/main/java/edu/helpdesk/signin/services/events/listeners/CorrectionRequestCreatedListener.java
+++ src/main/java/edu/helpdesk/signin/services/events/listeners/CorrectionRequestCreatedListener.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.helpdesk.signin.model.CorrectionRequest;
 import edu.helpdesk.signin.services.EmailService;
@@ -13,6 +14,8 @@ import edu.helpdesk.signin.services.events.CorrectionRequestCreatedEvent;
 public class CorrectionRequestCreatedListener implements ApplicationEventListener<CorrectionRequestCreatedEvent> {
 	private final Logger log = LoggerFactory.getLogger(CorrectionRequestCreatedListener.class);
 	
+	@Autowired
+	private EmailService emailService;
 	
 	public CorrectionRequestCreatedListener() {
 		log.debug("{} created", this.getClass().getSimpleName());
@@ -23,7 +26,7 @@ public class CorrectionRequestCreatedListener implements ApplicationEventListene
 			throws Exception {
 		final CorrectionRequest cr = event.getCreatedRequest();
 		String to = event.getCreatedRequest().getSubmitter().getNetId();
-		
+		String subject = String.format("%s, your correction request has been received", cr.getSubmitter().getFirstName());
 		String msg = String.format("%s, your correction request has been received, "
 				+ "and will change your work session from %s->%s to %s->%s if approved",
 				cr.getSubmitter().getFirstName(), 
@@ -31,11 +34,7 @@ public class CorrectionRequestCreatedListener implements ApplicationEventListene
 				cr.getOriginalSignoutTime(),
 				cr.getNewSigninTime(),
 				cr.getNewSignoutTime());
-		log.info("Email that should be sent to {}@rice.edu: \"{}\"", to, msg);
-		
-		new EmailService().send(new EmailService().createEmail(Arrays.asList(to + "@rice.edu"), "TESTING EMAIL", msg));
-		
-		// TODO actually send email
+		emailService.send(emailService.createEmail(Arrays.asList(to + "@rice.edu"), subject, msg));
 	}
 
 }
